699fedbdd4e2bd7a14ec3a61f422e97edf8d262e
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2020-12-30 08:00:06 +0000

    [Update]Wemory

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,5 +1,7 @@
 class Memory < ApplicationRecord
   
+  # 作成日時が新しい順に取り出す
+  default_scope -> {order(created_at: :desc)}
   belongs_to :pet
   has_many :memory_images, dependent: :destroy
   accepts_nested_attributes_for :memory_images, allow_destroy: true
