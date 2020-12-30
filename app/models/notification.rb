f3d52fda34e23f61333bd7ab78f1feb8065fe13f
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2020-12-30 01:19:41 +0000

    [Update]memory

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,8 @@
 class Notification < ApplicationRecord
 
-
+  # 作成日時が新しい順に取り出す
   default_scope -> {order(created_at: :desc)}
+  
   belongs_to :diary, optional: true
   belongs_to :diary_comment, optional: true
   belongs_to :visitor, class_name: 'Pet', foreign_key: 'visitor_id', optional: true
