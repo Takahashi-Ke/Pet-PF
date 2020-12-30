1bc342bfa051c5c5ec71eb77b94a734a6de4255c
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2020-12-30 01:18:26 +0000

    [Fix]refileエラー

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cf15f6a23fe6aa539c30dc48f7fb135b9e01d5bedb269fe8f06e46fc944041d92cd6237efad83caa3d79b6236c8f9314585213a0777c8b2884ab69c7b89ff6ef'
