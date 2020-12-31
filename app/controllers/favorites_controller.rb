ce74064b0672606ab9ec6baeb0000529949ee9bd
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2020-12-31 08:31:12 +0000

    [Update]favorite_memory

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,31 @@ class FavoritesController < ApplicationController
 
   def create
     pet = current_owner.pet
-    @diary = Diary.find(params[:id])
-    favorite = pet.favorites.new(diary_id: @diary.id)
-    favorite.save
-    @diary.create_notification_favo(pet)
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "diaries" || path[:controller] == "pets"
+      @diary = Diary.find(params[:id])
+      favorite = pet.favorites.new(diary_id: @diary.id)
+      favorite.save
+      @diary.create_notification_favo(pet)
+    else
+      @memory = Memory.find(params[:id])
+      favorite = pet.favorites.new(memory_id: @memory.id)
+      favorite.save
+      redirect_to request.referer
+    end
   end
 
   def destroy
-    @diary = Diary.find(params[:id])
     pet = current_owner.pet
-    favorite = pet.favorites.find_by(diary_id: @diary.id)
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "diaries" || path[:controller] == "pets"
+      @diary = Diary.find(params[:id])
+      favorite = pet.favorites.find_by(diary_id: @diary.id)
+    else
+      @memory = Memory.find(params[:id])
+      favorite = pet.favorites.find_by(memory_id: @memory.id)
+      redirect_to request.referer
+    end
     favorite.destroy
   end
 
