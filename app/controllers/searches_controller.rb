7fa7b49eed412ef847ca1fc6cba223e5617e2558
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-01 14:17:55 +0000

    [Update]search(検索機能)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,30 @@
 class SearchesController < ApplicationController
   
   def search
+    if params[:keyword].present?
+      @keyword = params[:keyword]
+      @pets = Pet.where("name LIKE?", "%#{@keyword}%")
+              # .joins(:pet_personalities).where(personality: @personalities)
+      @memories = Memory.where("title LIKE? OR body LIKE?", "%#{@keyword}%", "%#{@keyword}%")
+      @diaries = Diary.where("body LIKE?", "%#{@keyword}%")
+    end
+    if params[:types]
+      params[:types].each do |t|
+        @types = Pet.select(:type).where(type: t)
+      end
+    end
+    if params[:genders]
+      params[:genders].each do |g|
+        @genders = Pet.select(:gender).where(gender: g)
+      end
+    end
+    if params[:personalities]
+      params[:personalities].each do |pe|
+        @personalities = PetPersonality.where(personality: pe)
+      end
+    end
     
+    @diary = Diary.new
   end
   
 end
