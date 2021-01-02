09a550ace905365d91d4dde742a0438b8790455d
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-02 04:32:25 +0000

    [Update]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,34 @@
 class SearchesController < ApplicationController
   
   def search
+    # binding.pry
+    
     if params[:keyword].present?
       @keyword = params[:keyword]
-      @pets = Pet.where("name LIKE?", "%#{@keyword}%")
-              # .joins(:pet_personalities).where(personality: @personalities)
-      @memories = Memory.where("title LIKE? OR body LIKE?", "%#{@keyword}%", "%#{@keyword}%")
-      @diaries = Diary.where("body LIKE?", "%#{@keyword}%")
     end
-    if params[:types]
+    @pets = Pet.where("name LIKE?", "%#{@keyword}%").where.not(id: current_owner.pet) if params[:keyword].present?
+              # .where(type: params[:types])if params[:types].present?
+              # .where(gender: params[:genders])if params[:genders].present?
+            # .joins(:pet_personalities).where(personality: @personalities)
+    @memories = Memory.where("title LIKE? OR body LIKE?", "%#{@keyword}%", "%#{@keyword}%") if params[:keyword].present?
+                      # .where(pet_id: @pet.ids) if @pet.present?
+    @diaries = Diary.where("body LIKE?", "%#{@keyword}%")
+                    # .where(pet_id: @pet)
+    
+    
+    binding.pry
+    
+    if params[:types].present?
       params[:types].each do |t|
         @types = Pet.select(:type).where(type: t)
       end
     end
-    if params[:genders]
+    if params[:genders].present?
       params[:genders].each do |g|
         @genders = Pet.select(:gender).where(gender: g)
       end
     end
-    if params[:personalities]
+    if params[:personalities].present?
       params[:personalities].each do |pe|
         @personalities = PetPersonality.where(personality: pe)
       end
