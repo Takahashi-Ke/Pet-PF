17d920ffcbd79abaead54128d3b364da71ead15e
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-02 14:23:51 +0000

    [Fix]notification

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
+  get 'about' => 'homes#about'
   resources :owners, only: [:index] do
     member do
       get '/unsubscribe' => 'owners#unsubscribe'
