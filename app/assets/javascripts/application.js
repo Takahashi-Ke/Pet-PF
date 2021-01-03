0bf146b53ebcfbefef2db41e5f48c495accbcf03
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-03 19:26:10 +0000

    [Fix]ImagePreview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,20 +21,32 @@
 /* global $*/
 
 // 画像プレビュー
-$(document).on("turbolinks:load", function(){
+$(document).on("click", ".owner-image", function(){
   function readURL(input) {
     if(input.files && input.files[0]){
       var reader = new FileReader();
       reader.onload = function (e) {
-        $('.owner_prev').attr('src', e.target.result);
+          $('.owner-prev').attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
     }
   }
-  $("#owner_image").change(function(){
+  $(".owner-image").change(function(){
     readURL(this);
   });
-  $("#pet_image").change(function(){
+});
+$(document).on("click", ".pet-image", function(){
+  function readURL(input) {
+    if(input.files && input.files[0]){
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          $('.pet-prev').attr('src', e.target.result);
+        
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $(".pet-image").change(function(){
     readURL(this);
   });
 });
