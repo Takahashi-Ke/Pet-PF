6216b0fd5c568d30f6fa2b5c74fadc0456742acb
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-12 10:55:39 +0000

    [Update]bxslider/Topページ（スライドショー）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,15 @@
 class HomesController < ApplicationController
   
   def top
-    # photos = .where(created_at: 1.week.ago.all_day)
-    @random_photos = Diary.where.not(image_id: nil).order("RANDOM()").limit(10)
+    from = Time.current.at_beginning_of_day
+    to = (from + 6.day).at_end_of_day
+    @random_photos = Diary.where.not(image_id: nil).where(created_at: from...to).order("RANDOM()").limit(10)
     @diaries = Diary.where(created_at: Date.today.all_day)
-    @pets = Pet.all
-    
-    
+    #フォロワーランキング
+    follow_count_id_hash = Relationship.group(:follower_id).order('count_follower_id DESC').limit(5).count(:follower_id)
+    follow_count_id = follow_count_id_hash.keys
+    #フォロワー順にユーザーを取得する
+    @pets = Pet.where(id: follow_count_id).limit(10)
   end
   
 end
