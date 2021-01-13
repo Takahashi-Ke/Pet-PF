9403a60e404ae39af62618f6288ad3732675947e
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-13 15:18:17 +0000

    [Update]layout/レイアウト調整（退会画面メイン）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,7 +5,8 @@ class HomesController < ApplicationController
     to = Time.current.at_beginning_of_day
     from = (Time.current - 6.day).at_end_of_day
     # @random_photos = Diary.where.not(image_id: nil).where(created_at: from...to).order("RANDOM()").limit(10)
-    @random_photos = Diary.where.not(image_id: nil).where(created_at: from...to).order("RAND()").limit(10)
+    # mySQL対応(RAND)
+     @random_photos = Diary.where.not(image_id: nil).where(created_at: from...to).order("RAND()").limit(10)
     @diaries= Diary.where(created_at: Date.today.all_day)
     #フォロワーランキング
     follow_count_id_hash = Relationship.group(:follower_id).order('count_follower_id DESC').limit(5).count(:follower_id)
--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,7 @@ class Room < ApplicationRecord
   def create_notification_chat(pet, chat)
     chat_pet = PetRoom.find_by(room_id: id, pet_id: pet.id)
     notification = chat.pet.active_notifications.new(
-        room_id: id,
+        room_id: self.id,
         visited_id: chat_pet.pet_id,
         action: 'chat'
         )
