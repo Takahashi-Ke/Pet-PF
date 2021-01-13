cf14c1df0736494d4dae0321b1ad483845bfc243
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-13 16:13:54 +0000

    [Add]jscroll/本番環境テスト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -139,7 +139,12 @@ $(document).on('turbolinks:load', function() {
 
 // 無限スクロール
 $(document).on('turbolinks:load', function() {
-  $('.jscroll').infiniteScroll({
-    path: "nav.pagination a[rel=next]"
+  $('.jscroll').jscroll({
+    // 無限に追加する要素は、どこに入れる？
+    contentSelector: '.jscroll', 
+    // 次のページにいくためのリンクの場所は？ ＞aタグの指定
+    nextSelector: 'a.next',
+    // 読み込み中の表示はどうする？
+    loadingHtml: '読み込み中'
   });
 });
