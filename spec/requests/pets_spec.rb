6e21ded5f696956bdea1bd2e71d98a82418cbf70
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-01-31 12:05:57 +0000

    [Update]RSpec/requestテスト

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Pets", type: :request do
+RSpec.describe Pet, type: :request do
   before do
-    @pet = FactoryBot.create(:pet)
+    @pet = create(:pet)
   end
   
   describe "GET #show" do
@@ -12,17 +12,11 @@ RSpec.describe "Pets", type: :request do
     end
   end
   describe "GET #edit" do
-    it 'リクエストが成功すること' do
+    it '編集画面の表示に成功すること' do
       get edit_pet_path(@pet)
-      expect(response.status).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
-  # describe "GET #update" do
-  #   it 'マイページ画面の表示に成功すること' do
-  #     get pet_path(@pet)
-  #     expect(response).to have_http_status(302)
-  #   end
-  # end
   
   
 end
