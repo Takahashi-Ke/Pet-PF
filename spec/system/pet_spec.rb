4b7ed1bbcbe7290792dc5682f2106efd23b8458c
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-02-28 08:03:50 +0000

    [Update]rspec/テスト追加

--- spec/system/pet_spec.rb
+++ spec/system/pet_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe '', type: :system do
-  before do
-    @pet = create(:pet)
-  end
-  it 'ログイン画面が表示されること' do
-    visit '/sessions/new'
+RSpec.describe "Pet", type: :system do
+  let!(:owner) { attributes_for(:pet) }
+  binding.pry
+  describe 'オーナー、ペット情報編集テスト' do
+    before do
+      sign_in_as(owner)
+      visit pet_path(owner.pet)
+    end
+    context '編集に成功、マイページに遷移' do
+      it '正しく遷移されているか' do
+        click_on '編集'
+        visit edit_pet_path(ownet.pet)
+        expect(page).to have_content 'プロフィール編集'
+      end
+      it '編集に成功する' do
+        fill_in 'owner[name]', with: 'testowner'
+        fill_in 'owner[pet_attributes][name]', with: 'testpet'
+        fill_in 'owner[pet_attributes][introduction]', with: Faker::Lorem.characters(number: 100)
+        select 'おとなしい',from: 'owner[:pet_attributes][:pet_personalities]'
+        
+        click_button '更新する'
+        expect(current_path).to eq pet_path(pet)
+      end
+    end
   end
 end
