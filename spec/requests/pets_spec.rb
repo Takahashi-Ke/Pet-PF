db86879cb5e51a38a23ab74340f3b3ebfd773290
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-02-28 07:52:09 +0000

    [Finish]rspec/request/petテスト完了

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,21 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe Pet, type: :request do
-  before do
-    @pet = create(:pet)
+RSpec.describe "Pet", type: :request do
+  let!(:owner) { create(:owner) }
+  let!(:pet) { create(:pet, owner_id: owner.id) }
+  
+  describe "マイページ画面の表示(GET #show)" do
+    
+    context "未ログインの場合" do
+      it "ログインページへリダイレクトすること" do
+        get pet_path(pet)
+        expect(response).to redirect_to new_owner_session_path
+      end
+    end
+    
+    context "ログイン済みの場合" do
+      before do
+        sign_in owner
+      end
+      it "表示に成功すること" do
+        get pet_path(pet)
+      expect(response).to have_http_status(200)
+      end
+    end
+    
   end
   
-  describe "GET #show" do
-    it 'マイページ画面の表示に成功すること' do
-      get pet_path(@pet)
-      expect(response).to have_http_status(302)
+  describe "プロフィール編集画面の表示(GET #edit)" do
+        
+    context "未ログインの場合" do
+      it "ログインページへリダイレクトすること" do
+        get edit_pet_path(pet)
+        expect(response).to redirect_to new_owner_session_path
+      end
     end
-  end
-  describe "GET #edit" do
-    it '編集画面の表示に成功すること' do
-      get edit_pet_path(@pet)
-      expect(response).to have_http_status(302)
+    
+    context "ログイン済みの場合" do
+      before do
+        sign_in owner
+      end
+      it "表示に成功すること" do
+        get edit_pet_path(pet)
+      expect(response).to have_http_status(200)
+      end
     end
+    
   end
   
   
