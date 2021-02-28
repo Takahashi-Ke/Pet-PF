6c28e8bb56eff31416b1ffe0644e897ea1b5c1d2
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-02-28 08:02:03 +0000

    [Finish]rspec/request/ownerテスト完了

---
+++ spec/requests/owners_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Owner", type: :request do
+  let!(:owner) { create(:owner) }
+  let!(:pet) { create(:pet, owner_id: owner.id) }
+  
+  describe "退会画面の表示(GET #unsubscribe)" do
+    
+    context "未ログインの場合" do
+      it "ログインページへリダイレクトすること" do
+        get unsubscribe_owner_path(owner)
+        expect(response).to redirect_to new_owner_session_path
+      end
+    end
+    
+    context "ログイン済みの場合" do
+      before do
+        sign_in owner
+      end
+      it "表示に成功すること" do
+        get unsubscribe_owner_path(owner)
+        expect(response).to have_http_status(200)
+      end
+    end
+    
+  end
+    
+  describe "退会処理(DELETE #destroy)" do
+    
+    context "ログイン済みの場合" do
+      before do
+        sign_in owner
+      end
+      it "退会できるか" do
+        get unsubscribe_owner_path(owner)
+        delete owner_path(owner)
+        expect(Owner.find_by(id: owner.id)).to be_falsey
+      end
+    end
+    
+  end
+  
+end
