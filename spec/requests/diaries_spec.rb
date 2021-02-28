0ebf396791c7125ddf00aa894cd9f315a82b2b73
EC2 Default User <ec2-user@ip-172-31-20-29.ap-northeast-1.compute.internal>
2021-02-28 07:51:52 +0000

    [Finish]rspec/request/diaryテスト完了

--- spec/requests/diaries_spec.rb
+++ spec/requests/diaries_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Diaries", type: :request do
   let!(:owner) { create(:owner) }
   let!(:pet) { create(:pet, owner_id: owner.id) }
-  let!(:diary) { create(:diary, pet_id: pet.id, body: 'test_diary') }
+  let!(:diary) { create(:diary, pet_id: pet.id, body: "テスト日記") }
   
   describe "一覧画面の表示(GET #index)" do
     
@@ -18,7 +18,7 @@ RSpec.describe "Diaries", type: :request do
       before do
         sign_in owner
       end
-      it '表示に成功すること' do
+      it "表示に成功すること" do
         get diaries_path
       expect(response).to have_http_status(200)
       end
@@ -39,7 +39,7 @@ RSpec.describe "Diaries", type: :request do
       before do
         sign_in owner
       end
-      it '表示に成功すること' do
+      it "表示に成功すること" do
         get diary_path(diary)
         expect(response).to have_http_status(200)
       end
@@ -58,7 +58,7 @@ RSpec.describe "Diaries", type: :request do
         post diaries_path(pet_id: pet.id), params: { diary: { body: "日記のテスト",image: nil } }
         expect(Diary.find_by(pet_id: pet.id, body: "日記のテスト")).to be_truthy
       end
-      it '投稿後、一覧画面に遷移するか' do
+      it "投稿後、一覧画面に遷移するか" do
         post diaries_path(pet_id: pet.id), params: { diary: { body: "日記のテスト",image: nil } }
         expect(response).to redirect_to diaries_path
       end
@@ -73,11 +73,11 @@ RSpec.describe "Diaries", type: :request do
         sign_in owner
         get diaries_path
       end
-      it '日記を削除できるか' do
+      it "日記を削除できるか" do
         delete diary_path(diary)
-        expect(Diary.find_by(pet_id: pet.id, body: 'test_diary')).to be_falsey
+        expect(Diary.find_by(pet_id: pet.id, body: "テスト日記")).to be_falsey
       end
-      it '削除後、一覧画面に遷移するか' do
+      it "削除後、一覧画面に遷移するか" do
         delete diary_path(diary)
         expect(response).to redirect_to diaries_path
       end
